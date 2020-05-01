25c2f91f9f6d0163209aa90a9fdc2f606b074041
NikhilT27 <44368765+NikhilT27@users.noreply.github.com>
2020-05-01 23:32:18 +0530

    Chnge
    change

--- 01may2020.py
+++ 01may2020.py
@@ -1,5 +1,6 @@
 #  Link: https://www.hackerrank.com/challenges/sock-merchant/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
 # Nikhil Thakare
+#   fossnitrrcp31
 # -------------------------------------
 #
 #  Code:
