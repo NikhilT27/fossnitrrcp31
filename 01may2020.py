fc526a3e6982c69e23ed51435884064e33f2c62a
NikhilT27 <44368765+NikhilT27@users.noreply.github.com>
2020-05-01 23:29:52 +0530

    Create 01may2020.py

---
+++ 01may2020.py
@@ -0,0 +1,38 @@
+#  Link: https://www.hackerrank.com/challenges/sock-merchant/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+# Nikhil Thakare
+# -------------------------------------
+#
+#  Code:
+# !/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the sockMerchant function below.
+def sockMerchant(n, ar):
+    newList = list(ar)
+    unique = []
+    count = 0
+    for value in newList:
+        if value not in unique:
+            unique.append(value)
+    for unique_value in unique:
+        temp = newList.count(unique_value)
+        count += temp//2
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
